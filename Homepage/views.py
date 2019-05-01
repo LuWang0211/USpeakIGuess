39808ee4960559a35b4e9c8388c48c7ab4dfd47d
Lulu <luwangdudu@gmail.com>
2019-04-30 20:38:01 -0700

    homepage Chinese-English

--- Homepage/views.py
+++ Homepage/views.py
@@ -5,7 +5,22 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 def HomepageView(request):
     # return HttpResponse('This is the Home page of USpeakIGuess!')
-    return render(request, 'home.html')
+    selected_language=''
+    title='This is the Home page of USpeakIGuess!'
+    gotoFAQ='FQA'
+    gotoAId='AuthorIdentification'
+    if request.method == "POST":
+        selected_language=request.POST.get('selectelanguage')
+        if selected_language=='en-us':
+            title='Welcome the Home page of USpeakIGuess!'
+            gotoFAQ='FQA'
+            gotoAId='AuthorIdentification'
+        elif selected_language=='zh-cn':
+            title='欢迎来到“你说我猜”趣味语言分析平台!'
+            gotoFAQ='常见问题'
+            gotoAId='作者身份识别'
+        print(title, selected_language)
+    return render(request, 'home.html', {'title':title, 'FAQbuttonvalue':gotoFAQ, 'AIDbuttonvalue':gotoAId})
 
 def FAQpage(request):
     return HttpResponseRedirect('/faq/')
