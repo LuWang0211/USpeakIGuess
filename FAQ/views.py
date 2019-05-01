ed846157daa7a4f23509dffe1163284b435cd1a7
Lulu <luwangdudu@gmail.com>
2019-05-01 00:58:26 -0700

    more translations

--- FAQ/views.py
+++ FAQ/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.utils.translation import gettext as _
+
 
 # from django.http import HttpResponse
 from .models import FAQItems
@@ -10,8 +12,13 @@ from .models import FAQItems
 
 def FAQView(request):
     all_faq_items = FAQItems.objects.all()
+
+    important_text = _('lulu is small guaiguai')
+
+    print(important_text)
+    
     # all_FAQ_items ='cd  FAQItems.objects.all()'
-    return render(request, 'faq.html', {'all_faqitems': all_faq_items})
+    return render(request, 'faq.html', {'all_faqitems': all_faq_items, 'important_text': important_text})
 
 def addQuestion(request):
     new_item = request.GET['content']
