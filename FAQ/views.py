f09725a1e5dc9bde317bd880f19f4dd03f7a988f
Lulu <luwangdudu@gmail.com>
2019-05-01 14:30:15 -0700

    a little change-trans AId and Faq

--- FAQ/views.py
+++ FAQ/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.utils.translation import gettext as _
-
-
 # from django.http import HttpResponse
 from .models import FAQItems
 
@@ -12,13 +10,7 @@ from .models import FAQItems
 
 def FAQView(request):
     all_faq_items = FAQItems.objects.all()
-
-    important_text = _('lulu is small guaiguai')
-
-    print(important_text)
-    
-    # all_FAQ_items ='cd  FAQItems.objects.all()'
-    return render(request, 'faq.html', {'all_faqitems': all_faq_items, 'important_text': important_text})
+    return render(request, 'faq.html', {'all_faqitems': all_faq_items})
 
 def addQuestion(request):
     new_item = request.GET['content']
